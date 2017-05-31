7d1aac33a35a4f7c3abf84f38d840563359c448b
ianbrown80 <brown.i18@sky.com>
2017-05-31 23:33:31 +0100

    Front Page Styling Complete

---
+++ web/app/themes/bdpl/templates/front-page.php
@@ -0,0 +1,8 @@
+<div class="post">
+    <div class="post-header"><?php the_title(); ?></div>
+    <div class="post-info"><?php the_time('l jS F Y'); ?></div>      
+    <div class="post-content"><?php the_content(); ?></div>
+</div>
+
+
+<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
