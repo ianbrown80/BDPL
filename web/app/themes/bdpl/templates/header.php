103641b5208a2149a06ffab462124ee964baf3e3
ianbrown80 <brown.i18@sky.com>
2017-05-31 02:35:13 +0100

    Redone header

--- web/app/themes/bdpl/templates/header.php
+++ web/app/themes/bdpl/templates/header.php
@@ -1,11 +1,12 @@
 <header class="banner">
   <div class="container">
-    <a class="banner__image-link" href="http://placehold.it"><img class="banner__image" src="http://placehold.it/100x150"></a>
-    <h1 class="banner__heading"><?php bloginfo('name'); ?></h1>
-    <div class="banner__social-icons">
-      <a class="banner__social-link" href="http://placehold.it"><img class="banner__social-icon" src="http://placehold.it/80x80"></a>
-      <a class="banner__social-link" href="http://placehold.it"><img class="banner__social-icon" src="http://placehold.it/80x80"></a>
-      <a class="banner__social-link" href="http://placehold.it"><img class="banner__social-icon" src="http://placehold.it/80x80"></a>
+    <div class="row">
+      <div class="col-md-3">
+        <a href="<?php bloginfo('url'); ?>">
+          <h1 class="banner__heading"><?php bloginfo('name'); ?></h1>
+          <h3 class="banner__sub-heading"><?php bloginfo('description'); ?></h3>
+        </a>
+      </div>
     </div>
     <nav class="nav-primary">
       <?php
